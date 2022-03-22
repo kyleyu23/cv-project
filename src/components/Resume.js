5d92159747a4e9feee06115b85a8d5da1ef89efc
Kyle <kyleyu23@gmail.com>
2022-03-21 23:05:02 -0500

    refactor Resume.js to functional component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,58 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/Resume.css'
 
-class Resume extends Component {
-    constructor(props) {
-        super(props);
-    }
+function Resume(props) {
 
-    render() {
-        const
-            {
-                firstName, lastName, email, school,
-                major, dateOfStudy, companyName,
-                position, startingDate, endingDate,
-            }
-                = this.props.currentPerson;
+    const
+        {
+            firstName, lastName, email, school,
+            major, dateOfStudy, companyName,
+            position, startingDate, endingDate,
+        }
+            = props.currentPerson;
 
 
-        return (
-            <div className="resume">
-                <div className="section">
-                    <h1>{`${firstName} ${lastName}`}</h1>
-                    <p>{position}</p>
-                </div>
-
-                <div className="section">
-                    <h3> Personal Information</h3>
-                    <p>{email}</p>
-                    <p>Chicago, Illinois</p>
-                    <p>123-456-7890</p>
-                    <p>linkedin.com/in/johncook</p>
-                </div>
-
-                <div className="section">
-                    <h3> Experience</h3>
-                    <div className='headingWithDate'>
-                        <h4>{companyName}</h4>
-                        <p className="date">{`${startingDate}-${endingDate}`}</p>
-                    </div>
-                    <p className='position'>{position}</p>
-                    <p>Sunt proident amet commodo duis nisi ea. Ea laborum enim minim culpa et in irure dolor nostrud reprehenderit do. Excepteur exercitation nulla ullamco tempor sint irure aute pariatur exercitation. Do proident fugiat sunt nostrud consequat pariatur deserunt consequat Lorem sunt ipsum commodo.</p>
-                </div>
-
-                <div className="section">
-                    <h3> Education</h3>
-                    <div className='headingWithDate'>
-                        <h4>{school}</h4>
-                        <p className="date">{dateOfStudy}</p>
-                    </div>
-                    <div>{major}</div>
-                </div>
+    return (
+        <div className="resume">
+            <div className="section">
+                <h1>{`${firstName} ${lastName}`}</h1>
+                <p>{position}</p>
             </div>
 
-        )
-    }
+            <div className="section">
+                <h3> Personal Information</h3>
+                <p>{email}</p>
+                <p>Chicago, Illinois</p>
+                <p>123-456-7890</p>
+                <p>linkedin.com/in/johncook</p>
+            </div>
+
+            <div className="section">
+                <h3> Experience</h3>
+                <div className='headingWithDate'>
+                    <h4>{companyName}</h4>
+                    <p className="date">{`${startingDate}-${endingDate}`}</p>
+                </div>
+                <p className='position'>{position}</p>
+                <p>Sunt proident amet commodo duis nisi ea. Ea laborum enim minim culpa et in irure dolor nostrud reprehenderit do. Excepteur exercitation nulla ullamco tempor sint irure aute pariatur exercitation. Do proident fugiat sunt nostrud consequat pariatur deserunt consequat Lorem sunt ipsum commodo.</p>
+            </div>
+
+            <div className="section">
+                <h3> Education</h3>
+                <div className='headingWithDate'>
+                    <h4>{school}</h4>
+                    <p className="date">{dateOfStudy}</p>
+                </div>
+                <div>{major}</div>
+            </div>
+        </div>
+
+    )
 }
 
 export default Resume;
